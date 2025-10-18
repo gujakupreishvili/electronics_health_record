257623b6f464af6650087b934e424189b6bf0217
Luka Sulakvelidze <luka.sulakvelidze1@gmail.com>
2025-10-18 22:07:46 +0400

    feat: update health card schema and add new dependencies

--- src/db/dbUpdate.ts
+++ src/db/dbUpdate.ts
@@ -1,20 +1,20 @@
 import { db } from "@/lib/db";
 
-type PatientHistoryT = {
-  patientId: string;
-  cardNumber: string;
-  medicalCardCreationDate: Date;
-  clinicHospitalName: string;
-  location: string;
-  responsibleDoctorFullName: string;
-  hospitalizationDateTime: Date;
-  referralSource: string;
-  chiefComplaints: string;
-  finalClinicalDiagnosisMain: string;
-  doctorNotes: string[];
+type HealthCardT = {
+  patientId?: string;
+  cardNumber?: string;
+  medicalCardCreationDate?: Date;
+  clinicHospitalName?: string;
+  location?: string;
+  responsibleDoctorFullName?: string;
+  hospitalizationDateTime?: Date;
+  referralSource?: string;
+  chiefComplaints?: string;
+  finalClinicalDiagnosisMain?: string;
+  doctorNotes?: string[];
 };
 
-export const updatePatientHistory = (id: string, info: PatientHistoryT) => {
+export const updateHealthCard = (id: string, info: HealthCardT) => {
   db.transact(
     db.tx.healthCard[id].update({
       ...info,
