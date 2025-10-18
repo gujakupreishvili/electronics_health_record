6d5a1de9aea745a8fa9241ae86ed46007e2fde6c
Luka Sulakvelidze <luka.sulakvelidze1@gmail.com>
2025-10-18 21:35:31 +0400

    refactor: remove unused imports in dbDelete.ts

--- src/db/dbDelete.ts
+++ src/db/dbDelete.ts
@@ -1,7 +1,4 @@
-import schema from "@/instant.schema";
 import { db } from "@/lib/db";
-import { InstaQLEntity } from "@instantdb/react";
-
 
 export const deletePatient = (id: string) => {
   db.transact(db.tx.patients[id].delete());
